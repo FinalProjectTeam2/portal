a253f30d15bd2be8709130c22565a7262ec041d9
dntjr031 <63115449+dntjr031@users.noreply.github.com>
2020-07-27 23:19:55 +0900

    update

---
+++ src/main/webapp/resources/db/authority.sql
@@ -0,0 +1,3 @@
+INSERT INTO authority VALUES ('ADMIN','관리자', 'manager',1, SYSDATE);
+INSERT INTO authority VALUES ('ASSISTANT','부관리자', 'assistant manager',2, SYSDATE);
+INSERT INTO authority VALUES ('STAFF','스태프', 'staff',3, SYSDATE);
