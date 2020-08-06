099d15be272f98846df498c28b4f377e0e9f728b
dntjr031 <63115449+dntjr031@users.noreply.github.com>
2020-08-06 16:39:20 +0900

    udpate

--- src/main/java/com/will/portal/reply/model/ReplyVO.java
+++ src/main/java/com/will/portal/reply/model/ReplyVO.java
@@ -2,6 +2,11 @@ package com.will.portal.reply.model;
 
 import java.sql.Timestamp;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
 public class ReplyVO {
 	private int replyNo;
 	private String officialNo;
@@ -9,69 +14,7 @@ public class ReplyVO {
 	private String contents;
 	private int groupNo;
 	private int sortNo;
-	private int step;
 	private String delFlag;
 	private String postCode;
-	public int getReplyNo() {
-		return replyNo;
-	}
-	public void setReplyNo(int replyNo) {
-		this.replyNo = replyNo;
-	}
-	public String getOfficialNo() {
-		return officialNo;
-	}
-	public void setOfficialNo(String officialNo) {
-		this.officialNo = officialNo;
-	}
-	public Timestamp getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(Timestamp regDate) {
-		this.regDate = regDate;
-	}
-	public String getContents() {
-		return contents;
-	}
-	public void setContents(String contents) {
-		this.contents = contents;
-	}
-	public int getGroupNo() {
-		return groupNo;
-	}
-	public void setGroupNo(int groupNo) {
-		this.groupNo = groupNo;
-	}
-	public int getSortNo() {
-		return sortNo;
-	}
-	public void setSortNo(int sortNo) {
-		this.sortNo = sortNo;
-	}
-	public int getStep() {
-		return step;
-	}
-	public void setStep(int step) {
-		this.step = step;
-	}
-	public String getDelFlag() {
-		return delFlag;
-	}
-	public void setDelFlag(String delFlag) {
-		this.delFlag = delFlag;
-	}
-	public String getPostCode() {
-		return postCode;
-	}
-	public void setPostCode(String postCode) {
-		this.postCode = postCode;
-	}
-	@Override
-	public String toString() {
-		return "ReplyVO [replyNo=" + replyNo + ", officialNo=" + officialNo + ", regDate=" + regDate + ", contents="
-				+ contents + ", groupNo=" + groupNo + ", sortNo=" + sortNo + ", step=" + step + ", delFlag=" + delFlag
-				+ ", postCode=" + postCode + "]";
-	}
-	
 	
 }
