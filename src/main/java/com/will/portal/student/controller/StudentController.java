5416139b85a6cc2bece2b393d4e61ba937b60e61
jayhyun-hwang <67496978+jayhyun-hwang@users.noreply.github.com>
2020-08-13 19:59:49 +0900

    cc

--- src/main/java/com/will/portal/student/controller/StudentController.java
+++ src/main/java/com/will/portal/student/controller/StudentController.java
@@ -33,45 +33,69 @@ public class StudentController {
 		return "student/gradeCheck";
 	}
 
-
 	/**
 	 * 학생 성적 조회
+	 * 
 	 * @param principal
 	 * @param semester
 	 * @param model
 	 */
 	@RequestMapping("/studentScore")
 	public void studentScore(Principal principal, @RequestParam(required = false) String semester, Model model) {
-		MemberDetails user = (MemberDetails) ((Authentication)principal).getPrincipal();
-	    String officialNo = user.getOfficialNo();
-		
-		logger.info("studentScore, param: semester={}, officialNo={}",semester,officialNo);
+		MemberDetails user = (MemberDetails) ((Authentication) principal).getPrincipal();
+		String officialNo = user.getOfficialNo();
+
+		logger.info("studentScore, param: semester={}, officialNo={}", semester, officialNo);
 		List<String> slist = studentService.selectSemester(officialNo);
 		logger.info("slist={}", slist);
-		
-		ScoreSearchVO scoreSearchVo=new ScoreSearchVO();
+
+		List<List<Map<String, Object>>> tlist = new ArrayList<List<Map<String, Object>>>();
+		ScoreSearchVO scoreSearchVo = new ScoreSearchVO();
 		scoreSearchVo.setStuNo(officialNo);
-		scoreSearchVo.setSemester(semester);
-		List<Map<String, Object>> list = studentService.selectScore(scoreSearchVo);
-		logger.info("list={}", list);
+		if (semester == null || semester.isEmpty()) {
+			for (int i = 0; i < slist.size(); i++) {
+				scoreSearchVo.setSemester(slist.get(i));
+				List<Map<String, Object>> list = studentService.selectScore(scoreSearchVo);
+				tlist.add(list);
+			}
+		} else {
+
+			scoreSearchVo.setSemester(semester);
+			List<Map<String, Object>> list = studentService.selectScore(scoreSearchVo);
+			tlist.add(list);
+		}
+		logger.info("tlist={}", tlist);
 
 		List<String> slist2 = new ArrayList<String>();
 		String sort = "";
 		for (String sem : slist) {
 			if (sem.substring(sem.length() - 1).equals("2")) {
-				sort = "/1학기";
+				sort = "/1";
 			} else if (sem.substring(sem.length() - 1).equals("8")) {
-				sort = "/2학기";
+				sort = "/2";
 			}
 			slist2.add(sem.substring(0, 4) + sort);
 		}
 		logger.info("slist2={}", slist2);
 
-		model.addAttribute("slist",slist);
+		List<String> slistCh = new ArrayList<String>();
+		String sortCh = "";
+		for (String sem : slist) {
+			if (sem.substring(sem.length() - 1).equals("2")) {
+				sortCh = "01";
+			} else if (sem.substring(sem.length() - 1).equals("8")) {
+				sortCh = "02";
+			}
+			slistCh.add(sem.substring(0, 4) + sortCh);
+		}
+		logger.info("slistCh={}", slistCh);
+
+		model.addAttribute("slist", slist);
 		model.addAttribute("slist2", slist2);
-		model.addAttribute("list", list);
+		model.addAttribute("slistCh", slistCh);
+		model.addAttribute("tlist", tlist);
 	}
-	
+
 	@RequestMapping("/subjEval")
 	public void subjEval() {
 		logger.info("강의평가 화면 보여주기");
